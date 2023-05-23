7ae1b1dd75daf897fe8dc576b31963b0641c252a
Raphael Benoliel <raphael2gb@gmail.com>
2023-05-23 21:03:17 +0300

    fix Serach page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { sendLoginRequest } from '../../API/Auth_calls';
 import LoginView from './LoginView';
-import { use } from 'chai';
 import { useEffect } from 'react';
 
 function Login() {
