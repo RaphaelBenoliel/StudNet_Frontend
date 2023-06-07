fce857420f0d33031b6aa992f4e11da31b6d7855
Raphael Benoliel <raphael2gb@gmail.com>
2023-06-07 12:05:20 +0300

    continue Personal area page

--- src/pages/PersonalArea/PersonalArea.js
+++ src/pages/PersonalArea/PersonalArea.js
@@ -1,8 +1,7 @@
 /* eslint-disable */
 import React, { useEffect, useState } from 'react';
-import { Container, Heading, SubHeading, UserInfo, Button, List, ListItem, Label, FormContainer, FormLabel, FormInput}
- from './PersonalArea.style';
-
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
+import './PersonalArea.css';
 export default function PersonalArea() {
     
     const [followers, setFollowers] = useState([]);
@@ -30,24 +29,24 @@ export default function PersonalArea() {
     const fetchUserData = (userId) => {
       // Fetch user-related data from the backend server
       // Replace the placeholder API endpoints with the actual backend API endpoints
-      Promise.all([
-        fetch(`/api/users/${userId}/followers`),
-        fetch(`/api/users/${userId}/following`),
-        fetch(`/api/users/${userId}/liked-posts`),
-        fetch(`/api/users/${userId}/saved-posts`),
-        fetch(`/api/users/${userId}/statistics`),
-      ])
-        .then((responses) => Promise.all(responses.map((res) => res.json())))
-        .then(([followersData, followingData, likedPostsData, savedPostsData, statisticsData]) => {
-          setFollowers(followersData);
-          setFollowing(followingData);
-          setLikedPosts(likedPostsData);
-          setSavedPosts(savedPostsData);
-          setStatistics(statisticsData);
-        })
-        .catch((error) => {
-          console.error('Error fetching user data:', error);
-        });
+    //   Promise.all([
+    //     fetch(`/api/users/${userId}/followers`),
+    //     fetch(`/api/users/${userId}/following`),
+    //     fetch(`/api/users/${userId}/liked-posts`),
+    //     fetch(`/api/users/${userId}/saved-posts`),
+    //     fetch(`/api/users/${userId}/statistics`),
+    //   ])
+    //     .then((responses) => Promise.all(responses.map((res) => res.json())))
+    //     .then(([followersData, followingData, likedPostsData, savedPostsData, statisticsData]) => {
+    //       setFollowers(followersData);
+    //       setFollowing(followingData);
+    //       setLikedPosts(likedPostsData);
+    //       setSavedPosts(savedPostsData);
+    //       setStatistics(statisticsData);
+    //     })
+    //     .catch((error) => {
+    //       console.error('Error fetching user data:', error);
+    //     });
     };
   
     const editProfile = (event) => {
@@ -97,130 +96,180 @@ export default function PersonalArea() {
       };
 
     return (
-      <Container>
-        <Heading>Personal Area</Heading>
-  
-        {user && (
-          <div>
-            <SubHeading>User Profile</SubHeading>
-            <UserInfo>
-              <Label>Username:</Label>
-              <span>{user.username}</span>
-            </UserInfo>
-            <UserInfo>
-              <Label>Email:</Label>
-              <span>{user.email}</span>
-            </UserInfo>
-            <UserInfo>
-              <Label>Full Name:</Label>
-              <span>{user.fullName}</span>
-            </UserInfo>
-  
-            {!isEditing && (
-              <Button onClick={editProfile}>Edit Profile</Button>
+        <div className="App">
+            {user && (
+            <><div className='title'>
+                <p>
+                <img src={user.picture} alt="User Profile" />
+                
+                  {user.firstName} {user.lastName}
+                </p>
+                </div><Tabs>
+                    <TabList>
+                        <Tab>
+                            <p>Account</p>
+                        </Tab>
+                        <Tab>
+                            <p>Followers</p>
+                        </Tab>
+                        <Tab>
+                            <p>Following</p>
+                        </Tab>
+                        <Tab>
+                            <p>Title 4</p>
+                        </Tab>
+                        <Tab>
+                            <p>Title 5</p>
+                        </Tab>
+                    </TabList>
+                    <TabPanel>
+                        <div className="panel-content">
+                            <h2>Any content 1</h2>
+                        </div>
+                    </TabPanel><TabPanel>
+                        <div className="panel-content">
+                            <h2>Any content 2</h2>
+                        </div>
+                    </TabPanel><TabPanel>
+                        <div className="panel-content">
+                            <h2>Any content 3</h2>
+                        </div>
+                    </TabPanel><TabPanel>
+                        <div className="panel-content">
+                            <h2>Any content 4</h2>
+                        </div>
+                    </TabPanel><TabPanel>
+                        <div className="panel-content">
+                            <h2>Any content 5</h2>
+                        </div>
+                    </TabPanel>
+                </Tabs></>
             )}
-  
-            {isEditing && (
-              <FormContainer>
-                <form onSubmit={saveProfile}>
-                  <FormLabel>Username:</FormLabel>
-                  <FormInput
-                    type="text"
-                    name="username"
-                    value={updatedProfile.username}
-                    onChange={handleChange}
-                  />
-  
-                  <FormLabel>Email:</FormLabel>
-                  <FormInput
-                    type="email"
-                    name="email"
-                    value={updatedProfile.email}
-                    onChange={handleChange}
-                  />
-  
-                  <FormLabel>Full Name:</FormLabel>
-                  <FormInput
-                    type="text"
-                    name="fullName"
-                    value={updatedProfile.fullName}
-                    onChange={handleChange}
-                  />
-  
-                  <Button type="submit">Save</Button>
-                  <Button onClick={() => setIsEditing(false)}>Cancel</Button>
-                </form>
-              </FormContainer>
-            )}
-  
-            <Button onClick={deleteAccount}>Delete Account</Button>
-            <Button onClick={() => changePassword('newpassword')}>Change Password</Button>
-          </div>
-        )}
-  
-        {followers.length > 0 && (
-          <div>
-            <SubHeading>Followers ({followers.length})</SubHeading>
-            <List>
-              {followers.map((follower, index) => (
-                <ListItem key={index}>{follower}</ListItem>
-              ))}
-            </List>
-          </div>
-        )}
-  
-        {following.length > 0 && (
-          <div>
-            <SubHeading>Following ({following.length})</SubHeading>
-            <List>
-              {following.map((followedAccount, index) => (
-                <ListItem key={index}>{followedAccount}</ListItem>
-              ))}
-            </List>
-          </div>
-        )}
-  
-        {likedPosts.length > 0 && (
-          <div>
-            <SubHeading>Liked Posts ({likedPosts.length})</SubHeading>
-            <List>
-              {likedPosts.map((likedPost, index) => (
-                <ListItem key={index}>{likedPost}</ListItem>
-              ))}
-            </List>
-          </div>
-        )}
-  
-        {savedPosts.length > 0 && (
-          <div>
-            <SubHeading>Saved Posts ({savedPosts.length})</SubHeading>
-            <List>
-              {savedPosts.map((savedPost, index) => (
-                <ListItem key={index}>{savedPost}</ListItem>
-              ))}
-            </List>
-          </div>
-        )}
-  
-        {Object.keys(statistics).length > 0 && (
-          <div>
-            <SubHeading>Statistics</SubHeading>
-            <UserInfo>
-              <Label>Posts:</Label>
-              <span>{statistics.posts}</span>
-            </UserInfo>
-            <UserInfo>
-              <Label>Followers:</Label>
-              <span>{statistics.followers}</span>
-            </UserInfo>
-            <UserInfo>
-              <Label>Following:</Label>
-              <span>{statistics.following}</span>
-            </UserInfo>
-          </div>
-        )}
-      </Container>
+        </div>
     );
 }
-  
-  
+
+
+{/* <Container>
+<Heading>Personal Area</Heading>
+
+{user && (
+  <div>
+    <SubHeading>User Profile</SubHeading>
+    <UserInfo>
+      <Label>Username:</Label>
+      <span>{user.username}</span>
+    </UserInfo>
+    <UserInfo>
+      <Label>Email:</Label>
+      <span>{user.email}</span>
+    </UserInfo>
+    <UserInfo>
+      <Label>Full Name:</Label>
+      <span>{user.fullName}</span>
+    </UserInfo>
+
+    {!isEditing && (
+      <Button onClick={editProfile}>Edit Profile</Button>
+    )}
+
+    {isEditing && (
+      <FormContainer>
+        <form onSubmit={saveProfile}>
+          <FormLabel>Username:</FormLabel>
+          <FormInput
+            type="text"
+            name="username"
+            value={updatedProfile.username}
+            onChange={handleChange}
+          />
+
+          <FormLabel>Email:</FormLabel>
+          <FormInput
+            type="email"
+            name="email"
+            value={updatedProfile.email}
+            onChange={handleChange}
+          />
+
+          <FormLabel>Full Name:</FormLabel>
+          <FormInput
+            type="text"
+            name="fullName"
+            value={updatedProfile.fullName}
+            onChange={handleChange}
+          />
+
+          <Button type="submit">Save</Button>
+          <Button onClick={() => setIsEditing(false)}>Cancel</Button>
+        </form>
+      </FormContainer>
+    )}
+
+    <Button onClick={deleteAccount}>Delete Account</Button>
+    <Button onClick={() => changePassword('newpassword')}>Change Password</Button>
+  </div>
+)}
+
+{followers.length > 0 && (
+  <div>
+    <SubHeading>Followers ({followers.length})</SubHeading>
+    <List>
+      {followers.map((follower, index) => (
+        <ListItem key={index}>{follower}</ListItem>
+      ))}
+    </List>
+  </div>
+)}
+
+{following.length > 0 && (
+  <div>
+    <SubHeading>Following ({following.length})</SubHeading>
+    <List>
+      {following.map((followedAccount, index) => (
+        <ListItem key={index}>{followedAccount}</ListItem>
+      ))}
+    </List>
+  </div>
+)}
+
+{likedPosts.length > 0 && (
+  <div>
+    <SubHeading>Liked Posts ({likedPosts.length})</SubHeading>
+    <List>
+      {likedPosts.map((likedPost, index) => (
+        <ListItem key={index}>{likedPost}</ListItem>
+      ))}
+    </List>
+  </div>
+)}
+
+{savedPosts.length > 0 && (
+  <div>
+    <SubHeading>Saved Posts ({savedPosts.length})</SubHeading>
+    <List>
+      {savedPosts.map((savedPost, index) => (
+        <ListItem key={index}>{savedPost}</ListItem>
+      ))}
+    </List>
+  </div>
+)}
+
+{Object.keys(statistics).length > 0 && (
+  <div>
+    <SubHeading>Statistics</SubHeading>
+    <UserInfo>
+      <Label>Posts:</Label>
+      <span>{statistics.posts}</span>
+    </UserInfo>
+    <UserInfo>
+      <Label>Followers:</Label>
+      <span>{statistics.followers}</span>
+    </UserInfo>
+    <UserInfo>
+      <Label>Following:</Label>
+      <span>{statistics.following}</span>
+    </UserInfo>
+  </div>
+)}
+</Container> */}
