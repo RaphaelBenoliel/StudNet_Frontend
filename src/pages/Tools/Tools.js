640414d20fbadf7ceacecfb32af0ad05bbb535c5
ritavinitsky <rita.vinitsky@gmail.com>
2023-06-10 14:35:48 +0300

    .

--- src/pages/Tools/Tools.js
+++ src/pages/Tools/Tools.js
@@ -25,19 +25,19 @@ export default function Tools() {
       <h1 style={{ color: 'white' }}>General</h1>
       <br />
       <a href="https://openai.com/blog/chatgpt">
-      <img src={ChatGpt} alt="ChatGpt" style={{ width: 200, height: 200, margin: 30 }} />
+      <img src={ChatGpt} alt="ChatGpt" style={{ width: 150, height: 150, margin: 30 }} />
       </a>
       <a href="https://www.gool.co.il/">
-      <img src={Gool} alt="Gool" style={{ width: 200, height: 200, margin: 30 }} />
+      <img src={Gool} alt="Gool" style={{ width: 150, height: 150, margin: 30 }} />
       </a>
       <a href="https://translate.google.com/?hl=iw">
-      <img src={Translate} alt="Translate" style={{ width: 200, height: 200, margin: 20 }} />
+      <img src={Translate} alt="Translate" style={{ width: 150, height: 150, margin: 20 }} />
       </a>
       <a href="https://www.wikipedia.org/">
-      <img src={Wikipedia} alt="Wikipedia" style={{ width: 200, height: 200, margin: 30 }} />
+      <img src={Wikipedia} alt="Wikipedia" style={{ width: 150, height: 150, margin: 30 }} />
       </a>  
       <a href="https://zoom.us/">
-      <img src={Zoom} alt="Zoom" style={{ width: 200, height: 200, margin: 30 }} />
+      <img src={Zoom} alt="Zoom" style={{ width: 150, height: 150, margin: 30 }} />
       </a>
       <br />
       <br />
@@ -46,13 +46,13 @@ export default function Tools() {
       <h2 style={{ color: 'white' }}>Math</h2>
       <br />
       <a href="https://www.atlassian.com/software/jira">
-      <img src={Jira} alt="Jira" style={{ width: 200, height: 200, margin: 30 }} />
+      <img src={Jira} alt="Jira" style={{ width: 150, height: 150, margin: 30 }} />
       </a>
     <a href="https://github.com/">
-      <img src={GitHub} alt="GitHub" style={{ width: 200, height: 200, margin: 30 }} />
+      <img src={GitHub} alt="GitHub" style={{ width: 150, height: 150, margin: 30 }} />
     </a>
     <a href="https://qase.io/">
-      <img src={Qase} alt="Qase" style={{ width: 200, height: 200, margin: 30 }} />
+      <img src={Qase} alt="Qase" style={{ width: 150, height: 150, margin: 30 }} />
     </a>
     <br />
     <br />
@@ -61,29 +61,29 @@ export default function Tools() {
     <h3 style={{ color: 'white' }}>Software</h3>
     <br />
     <a href="https://stackoverflow.com/">
-      <img src={StackOverFlow} alt="StackOverFlow" style={{ width: 200, height: 200, margin: 20 }} />
+      <img src={StackOverFlow} alt="StackOverFlow" style={{ width: 150, height: 150, margin: 20 }} />
     </a>
     <a href="https://translate.google.com/?hl=iw">
-      <img src={Translate} alt="Translate" style={{ width: 200, height: 200, margin: 20 }} />
+      <img src={Translate} alt="Translate" style={{ width: 150, height: 150, margin: 20 }} />
     </a>
     <a href="https://discord.com/">
-      <img src={Discord} alt="Discord" style={{ width: 200, height: 200, margin: 30 }} />
+      <img src={Discord} alt="Discord" style={{ width: 150, height: 150, margin: 30 }} />
     </a>
     <a href="https://he.symbolab.com/">
-      <img src={Symbolab} alt="Symbolab" style={{ width: 200, height: 200, margin: 20 }} />
+      <img src={Symbolab} alt="Symbolab" style={{ width: 150, height: 150, margin: 20 }} />
     </a>
     <br />
     <br />
     <br />
     <br />
     <a href="https://he.symbolab.com/">
-      <img src={Symbolab} alt="Symbolab" style={{ width: 200, height: 200, margin: 20 }} />
+      <img src={Symbolab} alt="Symbolab" style={{ width: 150, height: 150, margin: 20 }} />
     </a>
     <a href="https://www.desmos.com/">
-      <img src={Desmos} alt="Desmos" style={{ width: 200, height: 200, margin: 20 }} />
+      <img src={Desmos} alt="Desmos" style={{ width: 150, height: 150, margin: 20 }} />
     </a>
     <a href="https://www.wikipedia.org/">
-      <img src={Wikipedia} alt="Wikipedia" style={{ width: 200, height: 200, margin: 30 }} />
+      <img src={Wikipedia} alt="Wikipedia" style={{ width: 150, height: 150, margin: 30 }} />
     </a>    
     <br />
     <br />
@@ -92,10 +92,10 @@ export default function Tools() {
     <h4 style={{ color: 'white' }}>Tools for online learning in groups</h4>
     <br />
     <a href="https://openai.com/blog/chatgpt">
-      <img src={ChatGpt} alt="ChatGpt" style={{ width: 200, height: 200, margin: 30 }} />
+      <img src={ChatGpt} alt="ChatGpt" style={{ width: 150, height: 150, margin: 30 }} />
       </a>
       <a href="https://www.gool.co.il/">
-      <img src={Gool} alt="Gool" style={{ width: 200, height: 200, margin: 30 }} />
+      <img src={Gool} alt="Gool" style={{ width: 150, height: 150, margin: 30 }} />
       </a>
     </div>
  );
